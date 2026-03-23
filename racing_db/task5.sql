ddeb996d28a362f050e4a8a39b2c5f78c7100d30
Timur Nurzhau <ali.zhau.new@gmail.com>
2026-03-23 21:45:24 +0300

    исправлен task5.sql: фильтрация только по автомобилям с avg_pos > 3.0

--- racing_db/task5.sql
+++ racing_db/task5.sql
@@ -17,11 +17,7 @@ WITH CarAvg AS (
          SELECT class FROM LowPosition WHERE low_count = (SELECT max_low FROM MaxLowCount)
      )
 SELECT ca.name AS car_name, ca.class AS car_class,
-       CASE
-           WHEN ca.avg_pos = 2.0 THEN FORMAT(ca.avg_pos, 4)
-           WHEN ca.avg_pos = FLOOR(ca.avg_pos) THEN CONCAT(FLOOR(ca.avg_pos), '.0000')
-           ELSE FORMAT(ca.avg_pos, 4)
-           END AS average_position,
+       FORMAT(ca.avg_pos, 4) AS average_position,
        ca.race_count, cl.country AS car_country,
        (SELECT COUNT(DISTINCT race) FROM Results WHERE car IN (SELECT name FROM Cars WHERE class IN (SELECT class FROM TargetClasses))) AS total_races,
        lp.low_count AS low_position_count
@@ -29,4 +25,5 @@ FROM CarAvg ca
          JOIN Classes cl ON ca.class = cl.class
          JOIN LowPosition lp ON ca.class = lp.class
 WHERE ca.class IN (SELECT class FROM TargetClasses)
-ORDER BY lp.low_count DESC, ca.class, ca.name;
+  AND ca.avg_pos > 3.0
+ORDER BY lp.low_count DESC, ca.avg_pos, ca.name;
