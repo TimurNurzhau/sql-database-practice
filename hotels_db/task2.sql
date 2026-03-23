1b777f90f56c94fe00f04615daeaa003f5b61adb
Timur Nurzhau <ali.zhau.new@gmail.com>
2026-03-23 22:28:07 +0300

    исправлен task2.sql: total_spent = SUM(price)

--- hotels_db/task2.sql
+++ hotels_db/task2.sql
@@ -1,5 +1,5 @@
 SELECT c.ID_customer, c.name, COUNT(b.ID_booking) AS total_bookings,
-       SUM(r.price * DATEDIFF(b.check_out_date, b.check_in_date)) AS total_spent,
+       SUM(r.price) AS total_spent,
        COUNT(DISTINCT h.ID_hotel) AS unique_hotels
 FROM Customer c
          JOIN Booking b ON c.ID_customer = b.ID_customer
@@ -7,5 +7,5 @@ FROM Customer c
          JOIN Hotel h ON r.ID_hotel = h.ID_hotel
 GROUP BY c.ID_customer
 HAVING COUNT(b.ID_booking) > 2 AND COUNT(DISTINCT h.ID_hotel) > 1
-   AND SUM(r.price * DATEDIFF(b.check_out_date, b.check_in_date)) > 500
-ORDER BY total_spent, c.name;
+   AND SUM(r.price) > 500
+ORDER BY total_spent;
