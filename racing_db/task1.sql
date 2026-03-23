03e2c6557779b1a2c2f5a4e87cd66731d23818b2
Timur Nurzhau <ali.zhau.new@gmail.com>
2026-03-23 21:26:54 +0300

    исправлен task1.sql: сортировка по позиции и году

--- racing_db/task1.sql
+++ racing_db/task1.sql
@@ -1,12 +1,12 @@
 WITH CarAvg AS (
-    SELECT c.name, c.class, AVG(r.position) AS avg_pos, COUNT(r.race) AS race_count
-    FROM Cars c
-             JOIN Results r ON c.name = r.car
-    GROUP BY c.name, c.class
-)
+    SELECT c.name, c.class, AVG(r.position) AS avg_pos, COUNT(r.race) AS race_count, MAX(c.year) AS year
+FROM Cars c
+    JOIN Results r ON c.name = r.car
+GROUP BY c.name, c.class
+    )
 SELECT ca.name AS car_name, ca.class AS car_class, ca.avg_pos AS average_position, ca.race_count
 FROM CarAvg ca
 WHERE (ca.class, ca.avg_pos) IN (
     SELECT class, MIN(avg_pos) FROM CarAvg GROUP BY class
 )
-ORDER BY ca.avg_pos, ca.name;
+ORDER BY ca.avg_pos, ca.year;
