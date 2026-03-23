ddd9dc7f8279f8f703b07a9e41bc7103052be87e
Timur Nurzhau <ali.zhau.new@gmail.com>
2026-03-23 21:28:48 +0300

    исправлен task3.sql: правильный подсчет total_races

--- racing_db/task3.sql
+++ racing_db/task3.sql
@@ -6,8 +6,12 @@ WITH ClassAvg AS (
 ),
      MinClassAvg AS (
          SELECT MIN(class_avg_pos) AS min_avg FROM ClassAvg
+     ),
+     TargetClasses AS (
+         SELECT class FROM ClassAvg WHERE class_avg_pos = (SELECT min_avg FROM MinClassAvg)
      )
-SELECT ca.name AS car_name, ca.class AS car_class, ca.avg_pos AS average_position, ca.race_count, cl.country AS car_country, total_races.total
+SELECT ca.name AS car_name, ca.class AS car_class, ca.avg_pos AS average_position, ca.race_count, cl.country AS car_country,
+       (SELECT COUNT(DISTINCT race) FROM Results WHERE car IN (SELECT name FROM Cars WHERE class IN (SELECT class FROM TargetClasses))) AS total_races
 FROM (
          SELECT c.name, c.class, AVG(r.position) AS avg_pos, COUNT(r.race) AS race_count
          FROM Cars c
@@ -15,9 +19,5 @@ FROM (
          GROUP BY c.name, c.class
      ) ca
          JOIN Classes cl ON ca.class = cl.class
-         JOIN ClassAvg ca2 ON ca.class = ca2.class
-         CROSS JOIN (
-    SELECT COUNT(DISTINCT race) AS total FROM Results
-) total_races
-WHERE ca2.class_avg_pos = (SELECT min_avg FROM MinClassAvg)
+WHERE ca.class IN (SELECT class FROM TargetClasses)
 ORDER BY ca.avg_pos, ca.name;
