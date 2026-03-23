5eeebd144fc8f0f72ea26713491c733b4ac6679b
Timur Nurzhau <ali.zhau.new@gmail.com>
2026-03-23 21:30:23 +0300

    исправлен task3.sql: total_races считаем для конкретных автомобилей

--- racing_db/task3.sql
+++ racing_db/task3.sql
@@ -1,23 +1,24 @@
-WITH ClassAvg AS (
-    SELECT c.class, AVG(r.position) AS class_avg_pos
+WITH CarAvg AS (
+    SELECT c.name, c.class, AVG(r.position) AS avg_pos, COUNT(r.race) AS race_count
     FROM Cars c
              JOIN Results r ON c.name = r.car
-    GROUP BY c.class
+    GROUP BY c.name, c.class
 ),
+     ClassAvg AS (
+         SELECT class, AVG(avg_pos) AS class_avg_pos
+         FROM CarAvg
+         GROUP BY class
+     ),
      MinClassAvg AS (
          SELECT MIN(class_avg_pos) AS min_avg FROM ClassAvg
-     ),
-     TargetClasses AS (
-         SELECT class FROM ClassAvg WHERE class_avg_pos = (SELECT min_avg FROM MinClassAvg)
      )
 SELECT ca.name AS car_name, ca.class AS car_class, ca.avg_pos AS average_position, ca.race_count, cl.country AS car_country,
-       (SELECT COUNT(DISTINCT race) FROM Results WHERE car IN (SELECT name FROM Cars WHERE class IN (SELECT class FROM TargetClasses))) AS total_races
-FROM (
-         SELECT c.name, c.class, AVG(r.position) AS avg_pos, COUNT(r.race) AS race_count
-         FROM Cars c
-                  JOIN Results r ON c.name = r.car
-         GROUP BY c.name, c.class
-     ) ca
+       (SELECT COUNT(DISTINCT race) FROM Results WHERE car IN (
+           SELECT name FROM CarAvg WHERE class IN (
+               SELECT class FROM ClassAvg WHERE class_avg_pos = (SELECT min_avg FROM MinClassAvg)
+           )
+       )) AS total_races
+FROM CarAvg ca
          JOIN Classes cl ON ca.class = cl.class
-WHERE ca.class IN (SELECT class FROM TargetClasses)
+WHERE ca.class IN (SELECT class FROM ClassAvg WHERE class_avg_pos = (SELECT min_avg FROM MinClassAvg))
 ORDER BY ca.avg_pos, ca.name;
