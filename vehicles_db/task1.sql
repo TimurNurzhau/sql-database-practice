d70af4ce8e44ed63f89ffb3e0fcb0fd9c3ba29c0
Timur Nurzhau <ali.zhau.new@gmail.com>
2026-03-23 20:31:32 +0300

    справлена неоднозначность в task1.sql (явное указание таблицы)

--- vehicles_db/task1.sql
+++ vehicles_db/task1.sql
@@ -1,5 +1,6 @@
 -- Задача 1: Найти производителей и модели мотоциклов с мощностью более 150 л.с., ценой менее 20000, тип Sport
-SELECT maker, model FROM Vehicle
-                             JOIN Motorcycle ON Vehicle.model = Motorcycle.model
-WHERE horsepower > 150 AND price < 20000 AND type = 'Sport'
+SELECT Vehicle.maker, Motorcycle.model
+FROM Vehicle
+         JOIN Motorcycle ON Vehicle.model = Motorcycle.model
+WHERE horsepower > 150 AND price < 20000 AND Motorcycle.type = 'Sport'
 ORDER BY horsepower DESC;
