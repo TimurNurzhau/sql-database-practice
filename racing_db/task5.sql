53b87053e64d3f9e67d022ebacb8acb6c6f5709d
Timur Nurzhau <ali.zhau.new@gmail.com>
2026-03-23 21:43:48 +0300

    исправлен task5.sql: правильная фильтрация и форматирование

--- racing_db/task5.sql
+++ racing_db/task5.sql
@@ -12,12 +12,21 @@ WITH CarAvg AS (
      ),
      MaxLowCount AS (
          SELECT MAX(low_count) AS max_low FROM LowPosition
+     ),
+     TargetClasses AS (
+         SELECT class FROM LowPosition WHERE low_count = (SELECT max_low FROM MaxLowCount)
      )
-SELECT ca.name AS car_name, ca.class AS car_class, ca.avg_pos AS average_position, ca.race_count, cl.country AS car_country,
-       (SELECT COUNT(DISTINCT race) FROM Results) AS total_races,
+SELECT ca.name AS car_name, ca.class AS car_class,
+       CASE
+           WHEN ca.avg_pos = 2.0 THEN FORMAT(ca.avg_pos, 4)
+           WHEN ca.avg_pos = FLOOR(ca.avg_pos) THEN CONCAT(FLOOR(ca.avg_pos), '.0000')
+           ELSE FORMAT(ca.avg_pos, 4)
+           END AS average_position,
+       ca.race_count, cl.country AS car_country,
+       (SELECT COUNT(DISTINCT race) FROM Results WHERE car IN (SELECT name FROM Cars WHERE class IN (SELECT class FROM TargetClasses))) AS total_races,
        lp.low_count AS low_position_count
 FROM CarAvg ca
          JOIN Classes cl ON ca.class = cl.class
          JOIN LowPosition lp ON ca.class = lp.class
-WHERE lp.low_count = (SELECT max_low FROM MaxLowCount)
+WHERE ca.class IN (SELECT class FROM TargetClasses)
 ORDER BY lp.low_count DESC, ca.class, ca.name;
